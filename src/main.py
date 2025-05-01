29fd5c98996856f1244b15b94e3611c8ac2b4374
Andrii Sonsiadlo <andrii.sonsiadlo@gmail.com>
2025-05-01 11:18:26 +0200

    Update imports and load UI files in `main.py`

--- src/main.py
+++ src/main.py
@@ -10,17 +10,19 @@ from kivy.uix.screenmanager import ScreenManager
 from kivy.core.window import Window
 
 from ui.screens.face_scanner.screen import FaceScanner
-# from ui.screens.model.learn_screen import LearningMode
-# from ui.screens.model.edit_screen import LearningEdit
-# from ui.screens.model.create_screen import LearningCreate
-# from ui.screens.person.add_screen import AddPerson
-# from ui.screens.person.edit_screen import EditPerson
-# from ui.screens.person.wanted_screen import *
-# from ui.screens.model.recycleview_create import *
 from ui.screens.face_scanner.webcamera_view import WebCameraView
+from ui.screens.model.learn_screen import LearningMode
+from ui.screens.model.edit_screen import LearningEdit
+from ui.screens.model.create_screen import LearningCreate
+from ui.screens.model.recycleview_create import *
+from ui.screens.person.add_screen import AddPerson
+from ui.screens.person.edit_screen import EditPerson
+from ui.screens.person.wanted_screen import *
+from ui.screens.person.recycleview import *
 from ui.screen_stack import ScreenStack
 from ui.drop_button import DropButton
 from ui.popups.delete import *
+from ui.widget_styles import *
 
 # loading ui files
 Builder.load_file("assets/ui/app_ui.kv")
@@ -28,16 +30,16 @@ Builder.load_file("assets/ui/widget_styles.kv")
 
 # screens
 Builder.load_file("assets/ui/facescanner_screen.kv")
-# Builder.load_file("assets/ui/addperson_screen.kv")
-# Builder.load_file("assets/ui/editperson_screen.kv")
-# Builder.load_file("assets/ui/wantedperson_screen.kv")
-# Builder.load_file("assets/ui/learningmode_screen.kv")
-# Builder.load_file("assets/ui/createmodel_screen.kv")
-# Builder.load_file("assets/ui/editmodel_screen.kv")
-#
-# # popups
-# Builder.load_file("assets/ui/my_popup.kv")
-# Builder.load_file("assets/ui/plot_popup.kv")
+Builder.load_file("assets/ui/addperson_screen.kv")
+Builder.load_file("assets/ui/editperson_screen.kv")
+Builder.load_file("assets/ui/wantedperson_screen.kv")
+Builder.load_file("assets/ui/learningmode_screen.kv")
+Builder.load_file("assets/ui/createmodel_screen.kv")
+Builder.load_file("assets/ui/editmodel_screen.kv")
+
+# popups
+Builder.load_file("assets/ui/my_popup.kv")
+Builder.load_file("assets/ui/plot_popup.kv")
 
 
 # Main Screen with navigation bar on top
--- src/ui/base_screen.py
+++ src/ui/base_screen.py
@@ -1,8 +1,6 @@
 from kivy.uix.screenmanager import Screen
-from kivy.clock import mainthread
-from abc import ABC, abstractmethod
 
-from src.utils.logger import AppLogger
+from core.logger import AppLogger
 
 
 class BaseScreen(Screen):
@@ -16,13 +14,13 @@ class BaseScreen(Screen):
         """Called when screen is entered"""
         pass
 
-    def show_error_popup(self, title: str, message: str = "") -> None:
+    def show_error(self, title: str, message: str = "") -> None:
         """Show error popup"""
         from classes.Popup.my_popup_warn import MyPopupWarn
         popup = MyPopupWarn(text=f"{title}\n{message}")
         popup.open()
 
-    def show_info_popup(self, title: str) -> None:
+    def show_info(self, title: str) -> None:
         """Show info popup"""
         from classes.Popup.my_popup_info import MyPopupInfo
         popup = MyPopupInfo(text=title)
