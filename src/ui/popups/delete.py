6cfe0b6e8d0493d00cdb3138448ccd6732fc066b
Andrii Sonsiadlo <andrii.sonsiadlo@gmail.com>
2025-05-04 10:42:40 +0200

    refactor: replace list deletion with service calls

--- src/ui/popups/delete.py
+++ src/ui/popups/delete.py
@@ -1,29 +1,24 @@
-# Copyright (C) 2021 Andrii Sonsiadlo
-
-from model.model_list import ModelList
-from person.person_list import PersonList
+from services import person_service, model_service
 from ui.popups.ask import AskPopup
 
 
 class DeletePersonPopup(AskPopup):
-    def __init__(self, **kwargs):
-        super().__init__(text="Are you sure you want to delete '" + self.selected.name + "'?",
+    def __init__(self, name: str, **kwargs):
+        super().__init__(text="Are you sure you want to delete '" + name + "'?",
                          **kwargs)
-        self.list = PersonList()
-        self.selected = self.list.get_selected()
+        self.name = name
 
     def yes_pressed(self):
-        self.list.delete_person(self.selected.name)
+        person_service.delete_person(self.name)
         super().yes_pressed()
 
 
 class DeleteModelPopup(AskPopup):
-    def __init__(self, **kwargs):
-        super().__init__(text="Are you sure you want to delete '" + self.selected.name + "'?",
+    def __init__(self, name: str, **kwargs):
+        super().__init__(text="Are you sure you want to delete '" + name + "'?",
                          **kwargs)
-        self.list = ModelList()
-        self.selected = self.list.get_selected()
+        self.name = name
 
     def yes_pressed(self):
-        self.list.delete_model(self.selected.name)
+        model_service.delete_model(self.name)
         super().yes_pressed()
