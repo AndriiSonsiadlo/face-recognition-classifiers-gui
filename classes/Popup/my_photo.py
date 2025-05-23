720efde88c43e08dedb21da3f7937fc6ca07bd46
Andrii Sonsiadlo <andrii.sonsiadlo@gmail.com>
2025-05-23 20:04:59 +0200

    ref: remove old code

--- classes/Popup/my_photo.py
+++
@@ -1,9 +0,0 @@
-# Copyright (C) 2021 Andrii Sonsiadlo
-
-class MyPhoto:
-	size = 0
-
-	def __init__(self, path: str = ''):
-		self.index = self.size
-		self.__class__.size = self.__class__.size + 1
-		self.path = path
